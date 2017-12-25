8f9b1d11cd1ea6e7939155a6c9470b8c8c871684
Tong <temberature@gmail.com>
2017-12-25 17:57:34 +0800

    清空 master。

--- config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  API_ROOT: '"https://easy-mock.com/mock/5a3917cec2c1eb01dcd314bc"'
-}
--- src/utils/customAxios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: process.env.API_ROOT,
-  timeout: 10000,
-  headers: {'X-Custom-Header': 'foobar'}
-})
